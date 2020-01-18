338d7f21b6e898f351e4a5d98fb6337ec00604e5
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-17 17:32:55 -0700

    Tried to fix the Link in MovieList.js by adding a Route rendering Movie and importing Movie to MovieList.js. It didn't work.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
+import Movie from "./Movie";
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -31,22 +32,26 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <Link className="movie-card" to={`/item-list/${movie.id}`}>
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
+    <div>
+      <Link className="movie-card" to={`/movies/${movie.id}`}>
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
         </div>
-      ))}
-    </Link>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </Link>
+
+      <Route exact path="/movies/:id" render={props => <Movie />} />
+    </div>
   );
 }
 
